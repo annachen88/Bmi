131518ac51f4ca394d8b98d1892805e7c4d8bb86
annachen88 <annachen880522@gmail.com>
2018-10-09 16:16:49 +0800

    AlertMessage

--- app/src/main/java/com/anna/bmi/MainActivity.java
+++ app/src/main/java/com/anna/bmi/MainActivity.java
@@ -1,29 +1,57 @@
 package com.anna.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void count(View view){
-        Log.d("MainActivity","bmi");
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
+
+    public void count(View view) {
+        Log.d("MainActivity", "bmi");
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
-        Log.d("MainActivity",w+"/"+h);
-        float weight = Float.parseFloat(w) ;
-        float height = Float.parseFloat(h) ;
-        float bmi = weight/(height*height);
-        Log.d("MainActivity",bmi+" ");
+        Log.d("MainActivity", w + "/" + h);
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity", bmi + " ");
+        Toast.makeText(this, "your bmi is " + bmi, Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage("your bmi is " + bmi)
+                .setTitle("BMI")
+                .setPositiveButton("ok", null)
+                .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edWeight.setText("");
+                        edHeight.setText("");
+                    }
+                })
+//                       )
+                .show();
+        //.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        edWeight.setText("");
+//                        edHeight.setText("");
+//                    }
+//                }
 
     }
 }
